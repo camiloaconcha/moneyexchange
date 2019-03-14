c643d18a14ae6eee6ca95eb6131d011ca0e435e1
CamiloConcha <camiloaconchaa@gmail.com>
2019-03-14 06:17:30 -0500

    Added container and header ,footer

--- moneyexchange/src/app/app.module.ts
+++ moneyexchange/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ExchangeComponent } from "./exchange/exchange.component";
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
 
 
 @NgModule({
-  declarations: [AppComponent, ExchangeComponent],
+  declarations: [AppComponent, ExchangeComponent, FooterComponent, HeaderComponent, ContainerComponent],
   imports: [BrowserModule,HttpClientModule,],
   providers: [],
   bootstrap: [AppComponent]
