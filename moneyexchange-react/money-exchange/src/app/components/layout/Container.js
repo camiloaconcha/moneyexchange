03b43e01be63f372752256b570e18e197b72e21d
CamiloConcha <camiloaconchaa@gmail.com>
2019-03-16 21:35:21 -0500

    Added Header And Footer to React ApP

---
+++ moneyexchange-react/money-exchange/src/app/components/layout/Container.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CurrencyExchange from "../CurrencyExchange";
+
+const AppContainer = () => (
+  <div className="container">
+    <CurrencyExchange />
+  </div>
+);
+
+export default AppContainer;
